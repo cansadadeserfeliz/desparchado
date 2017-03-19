2a0cdf8a65a34346bfb62f192a9b9a9bbd768ea6
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-03-19 13:38:31 -0500

    Show past events on place detail

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
 
 from .models import Place
 
@@ -15,6 +16,11 @@ class PlaceDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['events'] = self.get_object().event_set.filter(
+            event_date__gte=timezone.now(),
             is_published=True,
         ).all()[:9]
+        context['past_events'] = self.get_object().event_set.filter(
+            event_date__lt=timezone.now(),
+            is_published=True,
+        ).order_by('-event_date').all()[:9]
         return context
