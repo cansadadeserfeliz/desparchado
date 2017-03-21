f1fb822edfb1f70cfbd1a68635ae2d6285f379c2
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-03-20 22:03:13 -0500

    Show bookmarked events on user profile

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ from model_utils.models import TimeStampedModel
 
 
 class UserEventRelation(TimeStampedModel):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
-    event = models.ForeignKey('events.Event')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='events')
+    event = models.ForeignKey('events.Event', related_name='user_relation')
     is_bookmarked = models.BooleanField(default=False)
     is_visited = models.BooleanField(default=False)
 
--- users/views.py
+++ users/views.py
@@ -2,9 +2,20 @@ from django.views.generic import DetailView
 
 from django.contrib.auth import get_user_model
 
+from events.models import Event
+
 User = get_user_model()
 
 
 class UserDetailView(DetailView):
     context_object_name = 'user_object'
     model = User
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['bookmarked_events'] = Event.objects.filter(
+            is_published=True,
+            user_relation__user=self.get_object(),
+            user_relation__is_bookmarked=True,
+        ).distinct().all()
+        return context
