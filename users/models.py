68131e6728ed5d5b55b850a2228a75733a5777d1
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-03-20 21:19:56 -0500

    User-event relations model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+
+
+class UserEventRelation(TimeStampedModel):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    event = models.ForeignKey('events.Event')
+    is_bookmarked = models.BooleanField(default=False)
+    is_visited = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = _('User-Event Relation')
+        verbose_name_plural = _('User-Event Relations')
+        unique_together = ('user', 'event')
