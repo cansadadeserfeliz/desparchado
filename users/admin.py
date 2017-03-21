9b4f7f47e2b633874ba3c9a1789a319df36aff67
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-03-20 21:22:01 -0500

    User-event relations admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserEventRelation
+
+
+@admin.register(UserEventRelation)
+class UserEventRelationAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'event',
+        'is_bookmarked',
+        'is_visited',
+    )
+
+    def has_delete_permission(self, request, obj=None):
+        return False
