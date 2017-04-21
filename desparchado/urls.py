5e1f80887ee95f555f34b228ade8be6914d6a6df
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-04-21 13:01:39 -0500

    Static sitemap

--- desparchado/urls.py
+++ desparchado/urls.py
@@ -19,8 +19,10 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.sitemaps.views import sitemap
 
 from .views import HomeView
+from .sitemap import sitemaps
 
 
 urlpatterns = [
@@ -39,4 +41,7 @@ urlpatterns = [
 
     url(r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),  # admin site
+
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
