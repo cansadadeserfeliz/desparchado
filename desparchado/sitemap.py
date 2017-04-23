95e01ca79b9684fd78a5f845dc7faf635b414279
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-04-23 08:27:21 -0500

    Sitemap for event

--- desparchado/sitemap.py
+++ desparchado/sitemap.py
@@ -1,9 +1,11 @@
 from django.contrib import sitemaps
 from django.urls import reverse
 
+from events.models import Event
+
 
 class StaticViewSitemap(sitemaps.Sitemap):
-    priority = 0.5
+    priority = 0.7
     changefreq = 'daily'
 
     def items(self):
@@ -13,6 +15,18 @@ class StaticViewSitemap(sitemaps.Sitemap):
         return reverse(item)
 
 
+class EventSitemap(sitemaps.Sitemap):
+    changefreq = 'daily'
+    priority = 1
+
+    def items(self):
+       return Event.published.all()
+
+    def lastmod(self, item):
+       return item.event_date
+
+
 sitemaps = {
     'static': StaticViewSitemap,
+    'events': EventSitemap,
 }
