dd503955cf2e7b7644681abb70311d6377af8de6
Vera Mazhuga <vero4ka.ru@gmail.com>
2017-08-12 22:27:29 -0500

    Add login url

--- desparchado/settings.py
+++ desparchado/settings.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+from django.core.urlresolvers import reverse_lazy
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,6 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = reverse_lazy('users:login')
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
