16c8eebeee5858b0513421c881d11bb770796e39
Vera Mazhuga <vero4ka.ru@gmail.com>
2018-03-28 09:27:22 -0500

    Fix future events filter

--- events/models.py
+++ events/models.py
@@ -14,19 +14,21 @@ from desparchado.templatetags.desparchado_tags import format_currency
 class EventQuerySet(models.QuerySet):
     def future(self):
         return self.filter(
-            Q(event_date__gte=timezone.now()) |
             (
-                Q(event_end_date__isnull=False) &
-                Q(event_end_date__lte=timezone.now())
+                Q(event_date__gte=timezone.now()) &
+                Q(event_end_date__isnull=True)
+            ) | (
+                Q(event_end_date__gte=timezone.now())
             ),
         )
 
     def past(self):
         return self.exclude(
-            Q(event_date__gte=timezone.now()) |
             (
-                Q(event_end_date__isnull=False) &
-                Q(event_end_date__lte=timezone.now())
+                Q(event_date__gte=timezone.now()) &
+                Q(event_end_date__isnull=True)
+            ) | (
+                Q(event_end_date__gte=timezone.now())
             ),
         )
 
