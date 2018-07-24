f9c1018ff41f479c62bc4f5f55e7b7e2b94fcc4e
Vera Mazhuga <vero4ka.ru@gmail.com>
2018-07-23 22:37:48 -0500

    Add blog posts and static links to sitemap

--- desparchado/sitemap.py
+++ desparchado/sitemap.py
@@ -2,7 +2,9 @@ from django.contrib import sitemaps
 from django.urls import reverse
 
 from events.models import Event
+from events.models import Speaker
 from places.models import Place
+from blog.models import Post
 
 
 class StaticViewSitemap(sitemaps.Sitemap):
@@ -10,7 +12,15 @@ class StaticViewSitemap(sitemaps.Sitemap):
     changefreq = 'daily'
 
     def items(self):
-        return ['home', 'about']
+        return [
+            'home',
+            'about',
+            'blog:post_list',
+            'events:event_list',
+            'events:speaker_list',
+            'events:organizer_list',
+            'places:place_list',
+        ]
 
     def location(self, item):
         return reverse(item)
@@ -38,8 +48,31 @@ class PlaceSitemap(sitemaps.Sitemap):
         return item.created
 
 
+class PostSitemap(sitemaps.Sitemap):
+    changefreq = 'daily'
+    priority = 0.7
+
+    def items(self):
+        return Post.objects.all()
+
+    def lastmod(self, item):
+        return item.created
+
+
+class SpeakerSitemap(sitemaps.Sitemap):
+    changefreq = 'daily'
+    priority = 0.7
+
+    def items(self):
+        return Speaker.objects.all()
+
+    def lastmod(self, item):
+        return item.created
+
 sitemaps = {
     'static': StaticViewSitemap,
     'events': EventSitemap,
     'places': PlaceSitemap,
+    'posts': PostSitemap,
+    'speakers': SpeakerSitemap,
 }
