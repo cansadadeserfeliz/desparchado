7f8866030e469c6bcec9489d2f6130d9c879a6e9
Vera Mazhuga <vero4ka.ru@gmail.com>
2018-11-17 16:19:58 -0500

    About us vie test

--- desparchado/tests/test_views.py
+++ desparchado/tests/test_views.py
@@ -8,3 +8,9 @@ class HomeViewTestCase(WebTest):
     def testBlog(self):
         response = self.app.get(reverse('home'), status=200)
         self.assertContains(response, 'Eventos para usted')
+
+
+class AboutViewTestCase(WebTest):
+
+    def testBlog(self):
+        self.app.get(reverse('about'), status=200)
