d12e2750c0b064601ba3286200e462e6bfb9555d
Vera Mazhuga <vero4ka.ru@gmail.com>
2018-11-17 20:47:13 -0500

    Test events list

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -1,3 +1,6 @@
+import datetime
+from datetime import timedelta
+
 from django.core.urlresolvers import reverse
 
 from django_webtest import WebTest
@@ -11,9 +14,17 @@ class EventListView(WebTest):
     def setUp(self):
         self.first_event = EventFactory()
         self.second_event = EventFactory()
-        self.unpublished_event = EventFactory(is_published=False)
+        self.not_published_event = EventFactory(is_published=False)
+        self.not_approved_event = EventFactory(is_approved=False)
+        self.past_event = EventFactory(
+            event_date=datetime.datetime.now() - timedelta(days=1)
+        )
 
     def testBlog(self):
         response = self.app.get(reverse('events:event_list'), status=200)
         self.assertEqual(len(response.context['events']), 2)
         self.assertIn(self.first_event, response.context['events'])
+        self.assertIn(self.second_event, response.context['events'])
+        self.assertNotIn(self.not_published_event, response.context['events'])
+        self.assertNotIn(self.not_approved_event, response.context['events'])
+        self.assertNotIn(self.past_event, response.context['events'])
