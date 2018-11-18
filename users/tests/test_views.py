473b8733912e4066c30e435666a0c5ddd6a9d273
Vera Mazhuga <vero4ka.ru@gmail.com>
2018-11-18 16:02:12 -0500

    Test user events list

--- users/tests/test_views.py
+++ users/tests/test_views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 
 from django_webtest import WebTest
 
+from events.tests.factories import EventFactory
 from .factories import UserFactory
 
 
@@ -27,3 +28,37 @@ class UserDetailViewTest(WebTest):
         )
         self.assertEqual(response.context['user_object'], self.user)
         self.assertContains(response, self.user.first_name)
+
+
+class UserAddedEventsListViewTest(WebTest):
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.other_user = UserFactory()
+
+        self.first_event = EventFactory(created_by=self.user)
+        self.second_event = EventFactory(created_by=self.user)
+
+        self.other_user_event = EventFactory(
+            created_by=self.other_user
+        )
+
+    def test_redirects_for_non_authenticated_user(self):
+        response = self.app.get(
+            reverse('users:user_added_events_list'),
+            status=302
+        )
+        self.assertIn(reverse('users:login'), response.location)
+
+    def test_successfully_shows_user_events(self):
+        response = self.app.get(
+            reverse('users:user_added_events_list'),
+            user=self.user,
+            status=200
+        )
+
+        self.assertEqual(response.context['user_object'], self.user)
+        self.assertEqual(len(response.context['events']), 2)
+        self.assertIn(self.first_event, response.context['events'])
+        self.assertIn(self.second_event, response.context['events'])
+        self.assertNotIn(self.other_user_event, response.context['events'])
