a64a4f6bf1f36bfac8b779708da5bcbc20b1c65a
Vera Mazhuga <vero4ka.ru@gmail.com>
2019-01-09 21:36:43 -0500

    Fix typo

--- games/models.py
+++ games/models.py
@@ -20,7 +20,7 @@ class HuntingOfSnarkGame(TimeStampedModel):
         verbose_name='Juego para',
         max_length=255,
         blank=True,
-        help_text='Su nombre o el nombre de su amigo con el quien quiere jugar'
+        help_text='Su nombre o el nombre de su amigo con el que quiere jugar'
     )
     total_points = models.IntegerField(
         verbose_name='Cantidad de puntos',
