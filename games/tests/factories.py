9fab15afe84e810aa4bc96442ba9e865ee0f34f8
Vera Mazhuga <vero4ka.ru@gmail.com>
2019-06-22 17:33:26 -0500

    Basic test for games

---
+++ games/tests/factories.py
@@ -0,0 +1,41 @@
+import factory
+import factory.fuzzy
+
+from ..models import HuntingOfSnarkGame
+from ..models import HuntingOfSnarkCategory
+from ..models import HuntingOfSnarkCriteria
+
+
+class HuntingOfSnarkGameFactory(factory.django.DjangoModelFactory):
+    player_name = factory.fuzzy.FuzzyText(length=100)
+    total_points = factory.fuzzy.FuzzyInteger(low=1)
+
+    @factory.post_generation
+    def criteria(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for criteria in extracted:
+                self.criteria.add(criteria)
+
+    class Meta:
+        model = HuntingOfSnarkGame
+
+
+class HuntingOfSnarkCategoryFactory(factory.django.DjangoModelFactory):
+    name = factory.Sequence(lambda n: 'category title %d' % n)
+
+    class Meta:
+        model = HuntingOfSnarkCategory
+
+
+class HuntingOfSnarkCriteriaFactory(factory.django.DjangoModelFactory):
+    public_id = factory.Sequence(lambda n: n)
+    name = factory.fuzzy.FuzzyText(length=100)
+    category = factory.SubFactory(HuntingOfSnarkCategoryFactory)
+
+    class Meta:
+        model = HuntingOfSnarkCriteria
