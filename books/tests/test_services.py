a3bd2ae744bba73fe3d7faa20f008dcebb25b439
Vera Mazhuga <vero4ka.ru@gmail.com>
2019-07-21 20:40:21 -0500

    Fix tests

--- books/tests/test_services.py
+++ books/tests/test_services.py
@@ -1,10 +1,3 @@
 from django_webtest import WebTest
 
-from .
 
-
-class GoodreadsService(WebTest):
-
-    def test_get_book_info(self):
-        response = self.app.get(reverse('home'), status=200)
-        self.assertContains(response, 'AÑADIR EVENTO')
