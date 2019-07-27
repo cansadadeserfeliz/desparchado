f11c2b604c612af7bea87eac045f2fae174463f2
Vera Mazhuga <vero4ka.ru@gmail.com>
2019-07-27 07:07:42 -0500

    PEP8

--- books/views.py
+++ books/views.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView
 
 from .models import Book
 from news.models import PressArticle
-from news.models import MediaSource
 
 
 class HomeTemplateView(TemplateView):
