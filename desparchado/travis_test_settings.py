7535d5c46869ace46c39b0c3a966307d490ceac4
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-05-10 18:56:49 -0500

    Add separate test settings only for travis

---
+++ desparchado/travis_test_settings.py
@@ -0,0 +1,16 @@
+from desparchado.settings import *
+
+STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'desparchado_test',
+        'USER': 'desparchado_dev',
+        'PASSWORD': 'secret',
+        'HOST': '127.0.0.1',
+        'PORT': 5433,
+    }
+}
