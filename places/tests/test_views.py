0ac33fd29e82e86237c0274601b7b96d2aa9ee54
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-05-20 21:09:43 -0500

    Test autocomplete for places

--- places/tests/test_views.py
+++ places/tests/test_views.py
@@ -3,6 +3,7 @@ import pytest
 from django.urls import reverse
 
 from ..models import Place
+from .factories import PlaceFactory
 
 
 @pytest.mark.django_db
@@ -97,3 +98,40 @@ def test_successfully_update_place(django_app, user, place):
     assert place.name == 'Librería LERNER'
 
     assert place.get_absolute_url() in response.location
+
+
+@pytest.mark.django_db
+def test_successfully_get_place_wia_autocomplete(django_app, user):
+    place = PlaceFactory(name='Librería Nacional')
+    # Create a place that won't appear in search results
+    PlaceFactory(name='Museo de Arte Moderno')
+
+    search_term = 'Nacional'
+    response = django_app.get(
+        reverse('places:place_autocomplete'),
+        {'q': search_term},
+        user=user,
+        status=200,
+    )
+    response_json = response.json
+
+    assert 'results' in response_json
+    results = response_json['results']
+    assert len(results) == 1
+    assert results[0]['id'] == str(place.id)
+    assert results[0]['selected_text'] == place.name
+
+
+@pytest.mark.django_db
+def test_non_authenticated_user_cannot_get_place_wia_autocomplete(django_app):
+    PlaceFactory(name='Librería Nacional')
+    search_term = 'Nacional'
+    response = django_app.get(
+        reverse('places:place_autocomplete'),
+        {'q': search_term},
+        status=200,
+    )
+    response_json = response.json
+
+    assert 'results' in response_json
+    assert len(response_json['results']) == 0
