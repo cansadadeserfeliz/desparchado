897f44136a619ee5b2c53b02537ca06df38b2eb3
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-05-20 19:06:13 -0500

    Test updating places

--- places/tests/test_views.py
+++ places/tests/test_views.py
@@ -65,3 +65,35 @@ def test_successfully_create_place(django_app, user, city):
     place = Place.objects.first()
     assert place.created_by == user
     assert place.get_absolute_url() in response.location
+
+
+@pytest.mark.django_db
+def test_non_authenticated_user_cannot_update_place(django_app, place):
+    response = django_app.get(reverse('places:place_update', args=[place.id]), status=302)
+    assert reverse('users:login') in response.location
+
+
+@pytest.mark.django_db
+def test_successfully_update_place(django_app, user, place):
+    place.created_by = user
+    place.save()
+
+    response = django_app.get(
+        reverse('places:place_update', args=[place.id]),
+        user=user,
+        status=200,
+    )
+
+    form = response.forms['place_form']
+    form['name'] = 'Librería LERNER'
+
+    response = form.submit()
+    print(response)
+    assert response.status_code == 302
+
+    place.refresh_from_db()
+
+    # Name was changed
+    assert place.name == 'Librería LERNER'
+
+    assert place.get_absolute_url() in response.location
