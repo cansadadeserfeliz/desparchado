653c8b7fa631181744ab4c25d1d1436c772e0148
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-05-24 12:56:41 -0500

    Move editor fields to models since they require unique related names and add missing field attributes

--- history/models.py
+++ history/models.py
@@ -1,10 +1,10 @@
 import uuid
 
 from django.db import models
+from django.conf import settings
 
 from model_utils.models import TimeStampedModel
 
-from desparchado.models import EditorsModel
 
 DATETIME_PRECISION_CHOICES = (
     ('year', 'Año'),
@@ -15,20 +15,8 @@ DATETIME_PRECISION_CHOICES = (
 )
 
 
-class DatetimePrecisionField(models.CharField):
-    choices = DATETIME_PRECISION_CHOICES
-    max_length = 15
-
-
-class BaseHistoryModel(TimeStampedModel, EditorsModel):
+class HistoricalFigure(TimeStampedModel):
     token = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-    sources = models.TextField('Fuentes de la información', default='', blank=True)
-
-    class Meta:
-        abstract = True
-
-
-class HistoricalFigure(BaseHistoryModel):
     name = models.CharField(
         verbose_name='Nombre corto',
         max_length=255,
@@ -40,27 +28,42 @@ class HistoricalFigure(BaseHistoryModel):
         help_text='Por ejemplo, "Simón José Antonio de la Santísima Trinidad Bolívar '
                   'de la Concepción y Ponte Palacios y Blanco"',
     )
+    sources = models.TextField('Fuentes de la información', default='', blank=True)
 
     image = models.ImageField('Imagen', blank=True, null=True, upload_to='history/historical-figures')
     image_source_url = models.URLField('Enlace a la fuente de la imagen', null=True, blank=True)
 
     date_of_birth = models.DateTimeField(db_index=True)
-    date_of_birth_precision = DatetimePrecisionField()
+    date_of_birth_precision = models.CharField(max_length=15, choices=DATETIME_PRECISION_CHOICES)
 
     date_of_death = models.DateTimeField(db_index=True, blank=True, null=True)
-    date_of_death_precision = DatetimePrecisionField(blank=True, null=True)
+    date_of_death_precision = models.CharField(
+        max_length=15,
+        choices=DATETIME_PRECISION_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Creado por',
+        related_name='created_historical_figures',
+    )
 
     class Meta:
         verbose_name = 'Personaje histórico'
         verbose_name_plural = 'Personajes históricos'
 
 
-class Event(BaseHistoryModel):
+class Event(TimeStampedModel):
+    token = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     title = models.CharField(
         verbose_name='Título',
         max_length=500,
     )
     description = models.TextField('Descripción', default='', blank=True)
+    sources = models.TextField('Fuentes de la información', default='', blank=True)
 
     image = models.ImageField('Imagen', blank=True, null=True, upload_to='history/events')
     image_source_url = models.URLField('Enlace a la fuente de la imagen', null=True, blank=True)
@@ -70,8 +73,8 @@ class Event(BaseHistoryModel):
         db_index=True,
         help_text='p. ej. 23/11/2019 16:40',
     )
-    event_date_precision = DatetimePrecisionField()
-    location_name = models.CharField(default='', blank=True)
+    event_date_precision = models.CharField(max_length=15, choices=DATETIME_PRECISION_CHOICES)
+    location_name = models.CharField(max_length=500, default='', blank=True)
 
     event_end_date = models.DateTimeField(
         'Fecha final del evento',
@@ -80,12 +83,24 @@ class Event(BaseHistoryModel):
         db_index=True,
         help_text='p. ej. 23/11/2019 18:00 (opcional)',
     )
-    event_end_date_precision = DatetimePrecisionField(blank=True, null=True)
+    event_end_date_precision = models.CharField(
+        max_length=15,
+        choices=DATETIME_PRECISION_CHOICES,
+        blank=True,
+        null=True,
+    )
 
     historical_figures = models.ManyToManyField(
         'history.HistoricalFigure',
-        related_name='historical_events',
+        related_name='history_events',
         blank=True,
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Creado por',
+        related_name='created_history_events',
     )
 
     class Meta:
@@ -93,7 +108,8 @@ class Event(BaseHistoryModel):
         verbose_name_plural = 'Eventos históricos'
 
 
-class Post(BaseHistoryModel):
+class Post(TimeStampedModel):
+    token = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     TYPE_QUOTE = 'quote'
     TYPES = (
         (TYPE_QUOTE, 'Cita'),
@@ -107,7 +123,8 @@ class Post(BaseHistoryModel):
     )
 
     text = models.TextField('Texto')
-    location_name = models.CharField(default='', blank=True)
+    location_name = models.CharField(max_length=500, default='', blank=True)
+    sources = models.TextField('Fuentes de la información', default='', blank=True)
 
     image = models.ImageField('Imagen', blank=True, null=True, upload_to='history/posts')
     image_source_url = models.URLField('Enlace a la fuente de la imagen', null=True, blank=True)
@@ -119,7 +136,7 @@ class Post(BaseHistoryModel):
         db_index=True,
         help_text='p. ej. 23/11/2019 18:00 (opcional)',
     )
-    post_date_precision = DatetimePrecisionField(blank=True, null=True)
+    post_date_precision = models.CharField(max_length=15, choices=DATETIME_PRECISION_CHOICES, blank=True, null=True)
 
     historical_figure = models.ForeignKey(
         'history.HistoricalFigure',
@@ -127,4 +144,11 @@ class Post(BaseHistoryModel):
         blank=True,
         null=True,
         on_delete=models.PROTECT,
+    )
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING,
+        verbose_name='Creado por',
+        related_name='created_history_posts',
     )
