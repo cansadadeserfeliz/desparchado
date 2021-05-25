3972975fff46ff2aefd8825a9b201071f07d4d1a
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-05-24 21:41:08 -0500

    Don't show cities on every page in footer

--- desparchado/settings/base.py
+++ desparchado/settings/base.py
@@ -107,8 +107,6 @@ TEMPLATES = [
 
                 'social_django.context_processors.backends',
                 'social_django.context_processors.login_redirect',
-
-                'desparchado.context_processors.cities',
             ],
         },
     },
