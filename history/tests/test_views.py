0f69f030bb9c21d6f19557bdb2ae305d828df622
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-06-01 14:20:37 -0500

    Add basic test to show historical figures list

--- history/tests/test_views.py
+++ history/tests/test_views.py
@@ -6,3 +6,8 @@ from django.urls import reverse
 @pytest.mark.django_db
 def test_show_home_view(django_app):
     django_app.get(reverse('history:index'), status=200)
+
+
+@pytest.mark.django_db
+def test_show_historical_figure_list(django_app):
+    django_app.get(reverse('history:historical_figure_list'), status=200)
