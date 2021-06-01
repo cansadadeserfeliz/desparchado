b98b5537d7382f3482beee99b0f9f0018014e962
Navz19 <jdnaviad@unal.edu.co>
2021-06-01 00:42:39 -0500

    Add fixture history_historical_figure to conftest

--- history/tests/conftest.py
+++ history/tests/conftest.py
@@ -1,8 +1,13 @@
 import pytest
 
-from .factories import EventFactory
+from .factories import EventFactory, HistoricalFigureFactory
 
 
 @pytest.fixture
 def history_event():
     return EventFactory()
+
+
+@pytest.fixture
+def history_historical_figure():
+    return HistoricalFigureFactory()
