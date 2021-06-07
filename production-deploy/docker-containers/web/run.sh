41957eadf143df2fcbdb497ee6fb78d4e10a75a3
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-06-07 17:37:26 -0500

    This flag is used as a command name. Bad line break or missing

--- production-deploy/docker-containers/web/run.sh
+++ production-deploy/docker-containers/web/run.sh
@@ -7,8 +7,8 @@ uwsgi --chdir=/app \
     --env DJANGO_SETTINGS_MODULE=desparchado.settings.production \
     --master --pidfile=/tmp/project-master.pid \
     --socket=127.0.0.1:49152 \
-    --processes=5 \                 # number of worker processes
-    --uid=1000 --gid=2000 \         # if root, uwsgi can drop privileges
-    --harakiri=20 \                 # respawn processes taking more than 20 seconds
-    --max-requests=5000 \           # respawn processes after serving 5000 requests
-    --vacuum                        # clear environment on exit
+    --processes=5 \
+    --uid=1000 --gid=2000 \
+    --harakiri=20 \
+    --max-requests=5000 \
+    --vacuum
