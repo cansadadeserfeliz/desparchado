87c4fa2a1494387974e29b6b5d214fd56232d44d
Navz19 <jdnaviad@gmail.com>
2021-06-08 06:29:10 -0500

    add view EventsListView to views.py

--- history/views.py
+++ history/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import ListView
 
-from .models import HistoricalFigure
+from .models import HistoricalFigure, Event
 
 
 class HistoricalFigureListView(ListView):
     model = HistoricalFigure
+
+
+class EventsListView(ListView):
+    model = Event
