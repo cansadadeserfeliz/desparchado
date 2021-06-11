0f5e37dfa7403fa58c74b7fd2928a71e176e9aca
Navz19 <jdnaviad@gmail.com>
2021-06-10 21:56:45 -0500

    Add test_show_event_list and test_show_event_detail to test_view.py

--- history/tests/test_views.py
+++ history/tests/test_views.py
@@ -13,3 +13,17 @@ def test_show_historical_figure_list(django_app, history_historical_figure, hist
     response = django_app.get(reverse('history:historical_figure_list'), status=200)
     assert history_historical_figure.name in response
     assert history_historical_figure_without_image.name in response
+
+
+@pytest.mark.django_db
+def test_show_event_list(django_app, history_event):
+    response = django_app.get(reverse('history:event_list'), status=200)
+    assert history_event.title in response
+
+
+@pytest.mark.django_db
+def test_show_event_detail(django_app, history_event):
+    response = django_app.get(reverse('history:event_detail', kwargs={'token': history_event.token}), status=200)
+    assert history_event.title in response
+    assert history_event.description in response
+    # assert history_event.event_date in response
