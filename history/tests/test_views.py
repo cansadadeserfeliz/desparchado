0129710054caab144e65e60ee5a6db98106a0245
juan-navia <jdnaviad@gmail.com>
2021-07-22 23:42:46 -0500

    Add test for preloaded post list on index page

--- history/tests/test_views.py
+++ history/tests/test_views.py
@@ -64,3 +64,9 @@ def test_show_event_detail(django_app, history_event):
     response = django_app.get(reverse('history:event_detail', kwargs={'token': history_event.token}), status=200)
     assert history_event.title in response
     assert history_event.description in response
+
+
+@pytest.mark.django_db
+def test_show_post_preloaded_list(django_app, history_post):
+    response = django_app.get(reverse('history:index'), status=200)
+    assert history_post.historical_figure.name in response
