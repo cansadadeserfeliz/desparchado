724ec3824bcad21fe59096b222fd52d02f098ef1
juan-navia <jdnaviad@gmail.com>
2021-07-23 02:02:39 -0500

    Add a test that verifies status code 400 when a request to an empy page is made

--- history/tests/test_views.py
+++ history/tests/test_views.py
@@ -81,3 +81,8 @@ def test_posts_api_retrieve_page(django_app, history_post_batch):
 def test_posts_api_response_without_query_parameter(django_app, history_post_batch):
     django_app.get(reverse('history:api_post_list'),  status=422)
 
+
+@pytest.mark.django_db
+def test_posts_api_response_with_empty_page(django_app, history_post):
+    django_app.get(reverse('history:api_post_list') + '?page=2', status=400)
+
