ee23a135d4e5a02767f9bf0f623f95713df721cb
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2021-10-01 20:27:04 -0500

    Remove old django toolbar urls

--- desparchado/urls.py
+++ desparchado/urls.py
@@ -98,8 +98,4 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-
-        # For django versions before 2.0:
-        # url(r'^__debug__/', include(debug_toolbar.urls)),
-
     ] + urlpatterns
