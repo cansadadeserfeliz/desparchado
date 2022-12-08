91bf3d7e3e38d9e858c8bed0a4edfa3f6b90c511
Vera Mazhuga <2270752+cansadadeserfeliz@users.noreply.github.com>
2022-12-08 10:50:13 -0500

    Installed typescript loader and configured webpack loader for Django

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const BundleTracker = require('webpack-bundle-tracker');
 
 module.exports = {
-    entry: './desparchado/static/src/js/main.js',
+    entry: {
+        main: './desparchado/static/src/js/main',
+        dashboard: './desparchado/static/src/js/dashboard'
+    },
     output: {
-        filename: 'js/main.js',
+        filename: 'js/[name].js',
         path: path.resolve(__dirname, '../desparchado/static/dist'),
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'})
+    ],
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,4 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
+    devServer: {
+        static: './dist',
+    },
 });
