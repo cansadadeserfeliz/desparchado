b434e659e14d25d5ccab8755da1d60ebaa09c73a
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2024-04-07 18:35:52 -0500

    Show past events if search applied

--- events/views.py
+++ events/views.py
@@ -46,15 +46,15 @@ class EventListView(ListView):
     def get_queryset(self):
         queryset = Event.objects.published()
 
+        if self.city:
+            queryset = queryset.filter(place__city=self.city)
+
         if self.q:
             queryset = queryset.filter(
                 Q(title__icontains=self.q) | Q(description__icontains=self.q)
-            )
+            ).order_by('-event_date')
         else:
-            queryset = queryset.future()
-
-        if self.city:
-            queryset = queryset.filter(place__city=self.city)
+            queryset = queryset.future().order_by('event_date')
 
         return queryset.select_related('place')
 
