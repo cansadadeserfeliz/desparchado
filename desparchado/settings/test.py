66d509e40abc8a2714c836c20cfb537921b39099
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-04-18 15:56:59 -0500

    [FEATURE][TAS-76] Fixed uni tests with Django-Vite

--- desparchado/settings/test.py
+++ desparchado/settings/test.py
@@ -5,3 +5,7 @@ DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
 LOGGING['root']['handlers'] = ['console']
+
+# Indicates whether to serve assets via the ViteJS development server
+# or from compiled production assets
+DJANGO_VITE['default']['dev_mode'] = True
