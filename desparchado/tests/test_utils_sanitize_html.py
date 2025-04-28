de1e354e9b7f3d62889605ce31d5b0656f8366fb
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-04-27 19:35:49 -0500

    [FIX][TAS-85] Added test cased for linebreaks

--- desparchado/tests/test_utils_sanitize_html.py
+++ desparchado/tests/test_utils_sanitize_html.py
@@ -3,8 +3,12 @@ import pytest
 from ..utils import sanitize_html
 
 
-@pytest.mark.parametrize('input,output', [
+@pytest.mark.parametrize('input_html,output_html', [
     ('Line 1\nLine2', 'Line 1\nLine2'),
+    ('Line 1\r\nLine2', 'Line 1\r\nLine2'),
+    ('Line 1<br>Line2', 'Line 1<br>Line2'),
+    ('Line 1<p>Line2</p>', 'Line 1<p>Line2</p>'),
+    ('Line 1    Line2', 'Line 1    Line2'),
 ])
 def test_do_not_remove_linebreaks(input_html, output_html):
     assert sanitize_html(input_html) == output_html
