b197037081832859a779fa4ac1d35455e37c9cc7
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-04-27 19:31:22 -0500

    [FIX][TAS-85] Fixed: Redefining built-in 'input'

--- desparchado/tests/test_utils_sanitize_html.py
+++ desparchado/tests/test_utils_sanitize_html.py
@@ -6,5 +6,5 @@ from ..utils import sanitize_html
 @pytest.mark.parametrize('input,output', [
     ('Line 1\nLine2', 'Line 1\nLine2'),
 ])
-def test_do_not_remove_linebreaks(input, output):
-    assert sanitize_html(input) == output
+def test_do_not_remove_linebreaks(input_html, output_html):
+    assert sanitize_html(input_html) == output_html
