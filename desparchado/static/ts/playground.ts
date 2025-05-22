723aea1b9cbbafd85e0750fb2ab4e9cad5846658
Cristian Rodriguez <cristian6569@gmail.com>
2025-05-22 10:09:13 -0500

    [FEATURE][TAS-111] Intro section on playground moved to home on redesign   - Added responsive to header for specific cases

--- desparchado/static/ts/playground.ts
+++ desparchado/static/ts/playground.ts
@@ -1,12 +1,8 @@
 import '../styles/playground.scss';
-
   document.addEventListener('DOMContentLoaded', () => {
     requestAnimationFrame(() => {
       const header = document.getElementById('header');
-      console.log(header);
       const height = header?.offsetHeight;
-      console.log(height);
       document.body.style.setProperty('--header-height', `${height}px`);
-
     });
   });
