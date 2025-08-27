5479a58afe59608578088b26ed15f3b4b1d1d3af
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-08-26 20:11:51 -0500

    [FEATURE][NA] Added basic event detail page for playground

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from events.models import Event
 
 
 class HomeView(TemplateView, UserPassesTestMixin):
     template_name = 'playground/home.html'
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class EventDetailView(DetailView, UserPassesTestMixin):
+    template_name = 'playground/event_detail.html'
+    model = Event
 
     def test_func(self):
         return self.request.user.is_superuser
