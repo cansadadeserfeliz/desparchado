c1090fec7fdd3fa9c29ad3b5ef855216059121d5
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-08-30 20:27:19 -0500

    [FEATURE][TAS-130] Lint

--- desparchado/backends.py
+++ desparchado/backends.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 
+
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
