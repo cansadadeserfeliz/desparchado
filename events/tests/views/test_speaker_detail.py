64de4692154de736afd4d55c6041d9e1638fb36d
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-08-31 18:12:12 -0500

    [FEATURE][TAS-134] Add a 404 case for unknown slugs

--- events/tests/views/test_speaker_detail.py
+++ events/tests/views/test_speaker_detail.py
@@ -10,3 +10,8 @@ def test_successfully_show_details_of_speaker(django_app, speaker):
         reverse(VIEW_NAME, args=[speaker.slug]), status=200,
     )
     assert response.context['speaker'] == speaker
+
+
+@pytest.mark.django_db
+def test_speaker_detail_404(django_app):
+    django_app.get(reverse(VIEW_NAME, args=['no-such-speaker']), status=404)
