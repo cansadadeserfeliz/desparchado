24804b582cf8f405dc15cc42b7d5e32e55177297
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-08-31 21:40:22 -0500

    [FEATURE][TAS-134] Added double breaks and spaces adjacent to breaks to sanitizer tests

--- desparchado/tests/test_utils_sanitize_html.py
+++ desparchado/tests/test_utils_sanitize_html.py
@@ -4,13 +4,17 @@ from ..utils import sanitize_html
 
 
 @pytest.mark.parametrize(
-    'input_html,output_html',
+    "input_html,output_html",
     [
-        ('Line 1\nLine 2', 'Line 1\nLine 2'),
-        ('Line 1\r\nLine 2', 'Line 1\r\nLine 2'),
-        ('Line 1<br>Line 2', 'Line 1<br>Line 2'),
-        ('Line 1<p>Line 2</p>', 'Line 1<p>Line 2</p>'),
-        ('Line 1    Line 2', 'Line 1    Line 2'),
+        ("Line 1\nLine 2", "Line 1\nLine 2"),
+        ("Line 1\r\nLine 2", "Line 1\r\nLine 2"),
+        ("Line 1<br>Line 2", "Line 1<br>Line 2"),
+        ("Line 1<p>Line 2</p>", "Line 1<p>Line 2</p>"),
+        ("Line 1    Line 2", "Line 1    Line 2"),
+        ("Line 1\n\nLine 2", "Line 1\n\nLine 2"),
+        ("Line 1 \nLine 2", "Line 1 \nLine 2"),
+        ("Line 1\n Line 2", "Line 1\n Line 2"),
+        ('<p class="red">Line</p>', "<p>Line</p>"),
     ],
 )
 def test_do_not_remove_linebreaks(input_html, output_html):
