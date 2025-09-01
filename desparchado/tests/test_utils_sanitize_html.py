cac05e6fc892eceaf03a395b84a304db8bff8d5c
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-08-31 20:28:18 -0500

    [FEATURE][TAS-134] Fixed typos

--- desparchado/tests/test_utils_sanitize_html.py
+++ desparchado/tests/test_utils_sanitize_html.py
@@ -6,11 +6,11 @@ from ..utils import sanitize_html
 @pytest.mark.parametrize(
     'input_html,output_html',
     [
-        ('Line 1\nLine2', 'Line 1\nLine2'),
-        ('Line 1\r\nLine2', 'Line 1\r\nLine2'),
-        ('Line 1<br>Line2', 'Line 1<br>Line2'),
-        ('Line 1<p>Line2</p>', 'Line 1<p>Line2</p>'),
-        ('Line 1    Line2', 'Line 1    Line2'),
+        ('Line 1\nLine 2', 'Line 1\nLine 2'),
+        ('Line 1\r\nLine 2', 'Line 1\r\nLine 2'),
+        ('Line 1<br>Line 2', 'Line 1<br>Line 2'),
+        ('Line 1<p>Line 2</p>', 'Line 1<p>Line 2</p>'),
+        ('Line 1    Line 2', 'Line 1    Line 2'),
     ],
 )
 def test_do_not_remove_linebreaks(input_html, output_html):
