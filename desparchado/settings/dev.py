5ac9a1fd05a1fb12f64abf37794270c0a3b1bc0f
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-09-06 13:43:09 -0500

    [FEATURE][TAS-148] Removed sentry from development

--- desparchado/settings/dev.py
+++ desparchado/settings/dev.py
@@ -24,25 +24,3 @@ DEBUG_TOOLBAR_CONFIG = {
 # Indicates whether to serve assets via the ViteJS development server
 # or from compiled production assets
 DJANGO_VITE['default']['dev_mode'] = True   # noqa: F405
-
-# Sentry
-sentry_sdk.init(
-    dsn=getenvvar('SENTRY_CONFIG_DNS', 'not-set'),   # noqa: F405
-    integrations=[
-        DjangoIntegration(),
-    ],
-    environment='development',
-    # Add data like request headers and IP for users;
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/
-    # for more info
-    send_default_pii=True,
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for tracing.
-    traces_sample_rate=1.0,
-    # Set profile_session_sample_rate to 1.0 to profile 100%
-    # of profile sessions.
-    profile_session_sample_rate=1.0,
-    # Set profile_lifecycle to "trace" to automatically
-    # run the profiler on when there is an active transaction
-    profile_lifecycle="trace",
-)
