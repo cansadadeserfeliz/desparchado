fe1d18c5f8d2894838091e231f085ae4f76f0bac
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-09-06 12:08:59 -0500

    [FEATURE][TAS-148] Lint

--- dashboard/views/__init__.py
+++ dashboard/views/__init__.py
@@ -1,5 +1,6 @@
+# ruff: noqa: F401
 from .blaa_events import BlaaEventCreateView, BlaaEventsListView
+from .filbo_events import FilboEventFormView
 from .home import HomeView
 from .places import PlacesListView
 from .social import SocialPostsListView, social_events_source
-from .filbo_events import FilboEventFormView
