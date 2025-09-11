251d3e981f7a4b88396aeb679f9d86c789924756
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-09-10 19:32:55 -0500

    [FEATURE][NA] Calculate acvite users

--- dashboard/views/home.py
+++ dashboard/views/home.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
+
 from django.contrib.auth import get_user_model
 from django.db.models import Count, DateField
 from django.db.models.functions import Cast
 from django.views.generic import TemplateView
+from django.utils import timezone
 
 from dashboard.mixins import SuperuserRequiredMixin
 from events.models import Event, Organizer, Speaker
@@ -34,6 +37,9 @@ class HomeView(SuperuserRequiredMixin, TemplateView):
         context['speakers_without_image_count'] = Speaker.objects.filter(
             image='',
         ).count()
-        context['active_users_count'] = User.objects.filter(is_active=True).count()
+        context['active_users_count'] = User.objects.filter(
+            is_active=True,
+            last_login__gt=timezone.now() - timedelta(days=60),
+        ).count()
         context['places_count'] = Place.objects.count()
         return context
