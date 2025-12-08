6daffe586471de36a6f4b57c2f1651bf69db1543
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-12-07 22:01:46 -0500

    [FEATURE][TAS-179] Fix line length violation causing pipeline failure

--- events/management/commands/migrate_markdown_to_html.py
+++ events/management/commands/migrate_markdown_to_html.py
@@ -1,4 +1,4 @@
-import markdown  # noqa: F403 this package is deprecated, manual installation required to run this script
+import markdown  # deprecated, manual installation required to run this script
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
