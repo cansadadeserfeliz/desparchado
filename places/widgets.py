8325084a0cbf3360e71fdbcd7b20ec9e50beb00b
Vera <2270752+cansadadeserfeliz@users.noreply.github.com>
2025-12-10 22:09:40 -0500

    [FEATURE][TAS-180] Documented braking changes in Django 6.0

--- places/widgets.py
+++ places/widgets.py
@@ -2,6 +2,12 @@ from mapwidgets import GoogleMapPointFieldWidget
 
 
 class GoogleMapPointFieldFixedWidget(GoogleMapPointFieldWidget):
+    """This fix was added due to braking changes in Django 6.0:
+    the widget fails to display with a JS crash.
+
+    See https://github.com/erdem/django-map-widgets/issues/163
+    """
+
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
         context["name"] = name
